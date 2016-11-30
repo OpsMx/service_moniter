14a686b6836e2413be814cd81a0d901dd71ad3e7
phanikumarp <phanikumar518@gmail.com>
2016-11-30 15:37:33 +0530

    postsdb issue fixed

--- machineAgents/Agent.py
+++ machineAgents/Agent.py
@@ -1,10 +1,17 @@
 import time
 import subprocess
 
-checks = ["SystemCheck.py","NetworkCheck.py"]
+try:
+ import potsdb
+except:
+  subprocess.call("pip install potsdb",shell=True)
+
+checks = ["python /opt/agents/monitor/SystemCheck.py","python /opt/agents/monitor/NetworkCheck.py"]
 
 if __name__ == "__main__":
+     i = 4;	
      while True:
-	procs = [subprocess.Popen(['python', check]) for check in checks]
-	#       procs = [subprocess.Popen(['svn', 'update', repo]) for repo in repos]		       	
+	procs = [subprocess.Popen(check, shell=True) for check in checks]		       			       	
+        print "created subprocess" 
+        #i = i-1
         time.sleep(4)
