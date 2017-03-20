31a513e4058398fca44a13238d3202c9f1e1eda8
phanikumarp <phanikumarp@users.noreply.github.com>
2017-03-21 00:26:25 +0530

    Update packer_installer.sh

--- packer_installer.sh
+++ packer_installer.sh
@@ -11,6 +11,13 @@ sudo chmod 755 /etc/init.d/tcollector
 sudo update-rc.d tcollector defaults
 #service tcollector start
 
+echo "*********Installing old-script metrics*********"
+sudo apt-get install -y python-pip python-dev build-essential
+sudo pip install potsdb
+sudo pip install boto
+sudo wget -O /opt/agentinstall.sh https://raw.githubusercontent.com/OpsMx/server_monitor/master/agentinstall.sh && sudo bash /opt/agentinstall.sh
+
+
 echo "*********Installing Logstash*********"
 sudo echo 'deb http://packages.elastic.co/logstash/2.4/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D27D666CD88E42B4
