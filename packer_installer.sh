86e83c2d1b47fb8f2beb762e2d647b715c85973e
phanikumarp <phanikumarp@users.noreply.github.com>
2017-03-20 11:24:07 +0530

    Update packer_installer.sh

--- packer_installer.sh
+++ packer_installer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "*********Installing packages*********"
-sudo apt-get install -y python-pip python-dev build-essential
-sudo pip install potsdb
-sudo pip install boto
+#echo "*********Installing packages*********"
+#sudo apt-get install -y python-pip python-dev build-essential
+#sudo pip install potsdb
+#sudo pip install boto
 
 #echo "*********Installing apache2*********"
 #sudo apt-get update
@@ -38,14 +38,14 @@ sudo service logstash restart
 echo "*********Installing packetbeat*********"
 sudo wget -O /opt/packetbeat_install.sh https://raw.githubusercontent.com/OpsMx/server_monitor/master/packetbeat_install.sh && sudo chmod 777 /opt/packetbeat_install.sh && sudo bash /opt/packetbeat_install.sh
 echo
-echo "*********Installing sys-metrics*********"
-sudo wget -O /opt/agentinstall.sh https://raw.githubusercontent.com/OpsMx/server_monitor/master/agentinstall.sh && sudo bash /opt/agentinstall.sh
-echo 
+#echo "*********Installing sys-metrics*********"
+#sudo wget -O /opt/agentinstall.sh https://raw.githubusercontent.com/OpsMx/server_monitor/master/agentinstall.sh && sudo bash /opt/agentinstall.sh
+#echo 
 
-echo '#!/bin/sh -e'>/etc/rc.local
-echo 'sudo service logstash start'>>/etc/rc.local
-echo 'sudo service packetbeat start'>>/etc/rc.local
-echo 'sudo python /opt/tcollector/tcollector.py -H 52.8.104.253 -p 4343 -D '>>/etc/rc.local
-echo 'exit 0'>>/etc/rc.local
-echo "******* Copied init serivces *************************"
-echo 
+#echo '#!/bin/sh -e'>/etc/rc.local
+#echo 'sudo service logstash start'>>/etc/rc.local
+#echo 'sudo service packetbeat start'>>/etc/rc.local
+#echo 'sudo python /opt/tcollector/tcollector.py -H 52.8.104.253 -p 4343 -D '>>/etc/rc.local
+#echo 'exit 0'>>/etc/rc.local
+#echo "******* Copied init serivces *************************"
+#echo 
