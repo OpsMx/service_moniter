1637d3723541bc1dbf6ca2cc02129aa0f08794be
phanikumarp <phanikumar518@gmail.com>
2017-03-21 17:49:04 +0530

    changed apache2 read permission

--- packer_installer.sh
+++ packer_installer.sh
@@ -11,13 +11,13 @@ sudo chmod 755 /etc/init.d/tcollector
 sudo update-rc.d tcollector defaults
 #service tcollector start
 
-echo "*********Installing Logstash*********"
+echo "*********Installing Logstash ***********"
 sudo echo 'deb http://packages.elastic.co/logstash/2.4/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D27D666CD88E42B4
 sudo apt-get -y update
 sudo apt-get install -y logstash
 sudo wget -O  /etc/logstash/conf.d/logstash.conf https://raw.githubusercontent.com/OpsMx/service_moniter/master/opsmx_logstash.conf
-sudo chmod -R 777 /var/log/
+sudo chmod o+rx -R /var/log/apache2/
 sudo update-rc.d logstash defaults
 #sudo service logstash restart
 echo "********* Packer Installation Completed**********"
