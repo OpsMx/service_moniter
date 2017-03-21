d18f7304f1411346c4c1cbf04b66705a4ca57dc9
phanikumarp <phanikumarp@users.noreply.github.com>
2017-03-21 12:39:21 +0530

    Update packer_installer.sh

--- packer_installer.sh
+++ packer_installer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 echo "*********Downloading tcollector scripts*********"
 sudo wget -O /opt/tcollector_opsmx.tar https://rawgit.com/OpsMx/service_moniter/master/tcollector_opsmx.tar
 sudo tar -xvf /opt/tcollector_opsmx.tar -C /opt/
@@ -11,13 +10,6 @@ echo "*********Installing tcollector init scripts**************"
 sudo chmod 755 /etc/init.d/tcollector
 sudo update-rc.d tcollector defaults
 #service tcollector start
-
-echo "*********Installing old-script metrics*********"
-sudo apt-get install -y python-pip python-dev build-essential
-sudo pip install potsdb
-sudo pip install boto
-sudo wget -O /opt/agentinstall.sh https://raw.githubusercontent.com/OpsMx/server_monitor/master/agentinstall.sh && sudo bash /opt/agentinstall.sh
-
 
 echo "*********Installing Logstash*********"
 sudo echo 'deb http://packages.elastic.co/logstash/2.4/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
